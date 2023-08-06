dd8b03135fe964af5d8a0ee0585cbd06e16db62f
omletteey <Aom.natchanika@gmail.com>
2023-08-06 13:25:13 +0700

    first commit

---
+++ frontend/src/components/About/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+    return (
+        <>
+            <h1>Natchanika Kaoon</h1>
+            <div>
+                <h1>
+                   ID 65130167
+                </h1>
+            </div>
+            <div>
+                <img style={{width: "24rem"}} src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t39.30808-6/319030419_6142392289151585_7050396066009381395_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHCffyJn2HAW3vJpIUeyRJ0mDSVabaLaL2YNJVptotovQTMSGQSAhpRSnNo6YLJGRqkACKNBG9fN9CgpRGXn77w&_nc_ohc=sSxryzKvA8sAX-HdKZG&_nc_oc=AQlOVepf5XXRJJApugpfGhDoBf6DTcIshZ8KER4jQsAIL2a2oNzkaAcmObRxkqACPJSAj5ysTYOEvdb0_EpS4Lk0&_nc_ht=scontent.fbkk2-8.fna&oh=00_AfCBh-2gJZoLSkUHkn1ktD_7l_l2Z0eQAdtg9pMx0akL4Q&oe=64D3FAA2"/>
+            </div>
+        </>
+    )
+}
+export default About
